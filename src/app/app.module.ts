f65a2a013a29ef2379be510acfbcdc5809d3e8c3
johnnyseubert <johnny.seubert@pulsati.com.br>
2023-03-16 18:08:08 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImcComponent } from './pages/imc/imc.component';
+import { ResultadoComponent } from './pages/imc/components/resultado/resultado.component';
+import { DiretivasComponent } from './pages/diretivas/diretivas.component';
+import { EmitirEventosComponent } from './pages/emitir-eventos/emitir-eventos.component';
+import { FilhoComponent } from './pages/emitir-eventos/components/filho/filho.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ImcComponent, ResultadoComponent, DiretivasComponent, EmitirEventosComponent, FilhoComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
